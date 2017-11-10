a54d927e2702b74fd77ebb998285ef9eb209aa68
jdeitric <31959443+jdeitric@users.noreply.github.com>
2017-11-10 18:30:10 -0500

    Add project files.

---
+++ MvcMovie/Controllers/ReviewModelsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class ReviewModelsController : Controller
+    {
+        private readonly MvcMovieContext _context;
+
+        public ReviewModelsController(MvcMovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ReviewModels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ReviewModel.ToListAsync());
+        }
+
+        // GET: ReviewModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reviewModel = await _context.ReviewModel
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (reviewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(reviewModel);
+        }
+
+        // GET: ReviewModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReviewModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Review")] ReviewModel reviewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reviewModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reviewModel);
+        }
+
+        // GET: ReviewModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reviewModel = await _context.ReviewModel.SingleOrDefaultAsync(m => m.Id == id);
+            if (reviewModel == null)
+            {
+                return NotFound();
+            }
+            return View(reviewModel);
+        }
+
+        // POST: ReviewModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Review")] ReviewModel reviewModel)
+        {
+            if (id != reviewModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reviewModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewModelExists(reviewModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reviewModel);
+        }
+
+        // GET: ReviewModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reviewModel = await _context.ReviewModel
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (reviewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(reviewModel);
+        }
+
+        // POST: ReviewModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var reviewModel = await _context.ReviewModel.SingleOrDefaultAsync(m => m.Id == id);
+            _context.ReviewModel.Remove(reviewModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReviewModelExists(int id)
+        {
+            return _context.ReviewModel.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MvcMovie/Models/ReviewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class ReviewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Comment { get; set; }
+
+        public string MovieTitle { get; set; }
+    }
+}
